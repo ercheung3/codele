b953df147a2206f0dbd57e2d6ae8bf3d6463fadf
ercheung3 <ercheung3@gmail.com>
2022-04-25 16:59:39 -0400

    Added get route with outlet context

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
-import { Outlet, Link } from "react-router-dom";
-
+import { Outlet, Link, useOutletContext } from "react-router-dom";
+import { useState, useEffect } from "react";
 function App() {
+  const [questions, setQuestions] = useState([]);
+
+  const websiteURL = "http://localhost:8000/api/question";
+  const codewarsapi = "https://www.codewars.com/api/v1/code-challenges/";
+
+  const getQuestions = async () => {
+    try {
+      const questions = await fetch(`${websiteURL}`);
+      const parsedQuestions = await questions.json();
+      setQuestions(parsedQuestions);
+      console.log(questions);
+      console.log(parsedQuestions);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(getQuestions, []);
   return (
     <div className="App">
       <h1>Codele</h1>
@@ -10,7 +27,7 @@ function App() {
         <Link to="/daily">Daily</Link>
         <Link to="/questions">Questions</Link>
       </nav>
-      <Outlet />
+      <Outlet context={questions} />
     </div>
   );
 }
--- src/routes/QuestionsComponent/questionsComponent.jsx
+++ src/routes/QuestionsComponent/questionsComponent.jsx
@@ -1,7 +1,14 @@
-const QuestionsComponent = (props) => {
+import { useOutletContext } from "react-router-dom";
+
+const QuestionsComponent = () => {
+  const questions = useOutletContext();
+  console.log(questions);
   return (
     <div>
       <h2>ALL QUESTIONS</h2>
+      {questions.map((question) => {
+        return <p key={`${question.source}-${question.id}`}>{question.name}</p>;
+      })}
     </div>
   );
 };
